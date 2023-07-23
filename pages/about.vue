97f43fbcfdabbfb4ad21cbd57761ff3db3059d4f
Harshith Rao <harshithrao316@gmail.com>
2023-07-23 21:02:07 +0530

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptatibus facere quibusdam accusamus. Quaerat
+            mollitia quam ullam temporibus rem cumque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptatibus facere quibusdam accusamus. Quaerat
+            mollitia quam ullam temporibus rem cumque!</p>
+        <p>{{ data }}</p>
+    </div>
+</template>
+
+<script setup>
+    const { data } = await useFetch('/api/currency/INR');
+</script>
+
+<style scoped>
+h1 {
+    margin-top: 30px;
+    font-size: 32px;
+}
+
+p {
+    margin-top: 35px;
+    font-size: 16px;
+}</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Hello</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptatibus facere quibusdam accusamus. Quaerat mollitia quam ullam temporibus rem cumque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptatibus facere quibusdam accusamus. Quaerat mollitia quam ullam temporibus rem cumque!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h1{
+        margin-top: 30px;
+        font-size: 32px;
+    }
+    p{
+        margin-top: 35px;
+        font-size: 16px;
+    }
+</style>
